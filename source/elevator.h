970a89a6d88ae91f50045781530535173d030ad2
Atle Bystrøm <aabystro@stud.ntnu.no>
2025-02-28 17:09:57 +0100

    merget

--- source/elevator.h
+++ source/elevator.h
@@ -1,1 +1,19 @@
+#ifndef ORDER_H
+#define ORDER_H
 
+#include <stdbool.h>
+
+#define N_FLOORS 4
+
+extern int direction;
+extern int currentFloor;
+
+void order_add_cabin(int floor);
+void order_add_hall(int floor, MotorDirection direction);
+void order_remove(int floor);
+bool order_exists();
+int order_next(int currentFloor, MotorDirection direction);
+void order_handle();
+int exec_scan_orders();
+
+#endif // ORDER_H
--- source/fsm.c
+++ source/fsm.c
@@ -2,88 +2,83 @@
 #include <stdbool.h>
 #include <unistd.h> 
 #include "fsm.h"
+#include "elevio.h"
 
 
 ElevatorState currentState = INITIALIZING;
-int currentFloor = -1; 
+int currentFloor = -1;
 bool stopButtonPressed = false;
 bool obstructionDetected = false;
 int destinationFloor = 1;
+int lastFloor = -1;
 
-int exec_scan_orders() {
 
-    return -1;
-}
 
 void elevator_fsm() {
     switch (currentState) {
         case INITIALIZING:
-            printf("Heis: Søker etter etasje");
-            currentFloor = 1; 
+            printf("Heis: Søker etter etasje\n");
+            elevio_motorDirection(DIRN_DOWN);
+            currentFloor = elevator_get_current_floor();
+            if (currentFloor != -1) {
             currentState = AT_FLOOR;
+            }
             break;
 
         case AT_FLOOR:
-            printf("Heis: Står stille i etasje %d. Venter på bestilling.", currentFloor);
-            destinationFloor = exec_scan_orders(); // Sjekker bestillinger
-            
+            printf("Heis: Står stille i etasje %d. Venter på bestilling.\n", currentFloor);
+            destinationFloor = exec_scan_orders();
+            currentFloor = elevator_get_current_floor();
             if (destinationFloor != -1) {
                 if (currentFloor < destinationFloor) {
                     currentState = MOVING_UP;
                 } else if (currentFloor > destinationFloor) {
                     currentState = MOVING_DOWN;
                 }
-            }
+        }
             break;
 
         case MOVING_UP:
-            printf("Heis: Kjører oppover");
-            sleep(1);
-            currentFloor++;
-            
-            if (currentFloor == destinationFloor) {
-                currentState = AT_FLOOR;
+            printf("Heis: Kjører oppover\n");
+            destinationFloor = exec_scan_orders();
+            elevio_motorDirection(DIRN_UP);
+            while (currentFloor < destinationFloor) {
+                currentFloor = elevio_floorSensor();
+                if (currentFloor == destinationFloor) {
+                    elevio_motorDirection(DIRN_STOP);
+                    currentState = AT_FLOOR;
+                    break;
+                }
             }
             break;
 
         case MOVING_DOWN:
-            printf("Heis: Kjører nedover");
-            sleep(1);
-            currentFloor--;
-            
-            if (currentFloor == destinationFloor) {
-                currentState = AT_FLOOR;
+            printf("Heis: Kjører nedover\n");
+            destinationFloor = exec_scan_orders();
+            elevio_motorDirection(DIRN_DOWN);
+            while (currentFloor > destinationFloor) {
+                currentFloor = elevio_floorSensor();
+                if (currentFloor == destinationFloor) {
+                    elevio_motorDirection(DIRN_STOP);
+                    currentState = AT_FLOOR;
+                        break;
+                }
             }
             break;
-
-        /*case DOOR_OPEN:
-            printf("Heis: Åpner døren i etasje %d.", currentFloor);
-            sleep(3); 
-            
-            if (!obstructionDetected) {
-                currentState = IDLE;
-            } else {
-                currentState = OBSTRUCTION;
-            }
-            break;
-
-        case OBSTRUCTION:
-            printf("Heis: Obstruksjon oppdaget! Døren forblir åpen.");
-            if (!obstructionDetected) {
-                printf("Heis: Obstruksjon fjernet, lukker døren.");
-                currentState = DOOR_OPEN;
-            }
-            break;*/
 
         case EMERGENCY_STOP_FLOOR:
-            printf("Heis: Nødstopp aktivert! Står stille.");
+            printf("Heis: Nødstopp aktivert! Står stille.\n");
+            elevio_motorDirection(DIRN_STOP);
+            currentFloor = elevio_floorSensor();
             if (!stopButtonPressed && currentFloor != -1) {
                 currentState = INITIALIZING;
             }
             break;
 
         case EMERGENCY_STOP_SHAFT:
-            printf("Heis: Nødstopp aktivert! Står stille i shaft.");
+            printf("Heis: Nødstopp aktivert! Står stille i shaft.\n");
+            elevio_motorDirection(DIRN_STOP);
+            currentFloor = elevio_floorSensor();
             if (!stopButtonPressed && currentFloor == -1) {
                 currentState = INITIALIZING;
             }
@@ -92,11 +87,15 @@ void elevator_fsm() {
 }
 
 void elevator_init() {
+    elevio_init();
+    while(elevio_floorSensor() == -1) {
+        elev_set_motor_direction(DIRN_DOWN);
+    }
+    elev_set_motor_direction(DIRN_STOP);
     currentState = INITIALIZING;
-    currentFloor = -1;
+    currentFloor = elevio_floorSensor();
     stopButtonPressed = false;
     obstructionDetected = false;
-    destinationFloor = 1;
 }
 
 void elevator_request_floor(int floor) {
@@ -105,7 +104,7 @@ void elevator_request_floor(int floor) {
 
 void elevator_stop() {
     stopButtonPressed = true;
-    currentState = EMERGENCY_STOP;
+    currentState = EMERGENCY_STOP_FLOOR;
 }
 
 void elevator_clear_stop() {
@@ -122,10 +121,15 @@ ElevatorState elevator_get_state() {
 }
 
 int elevator_get_current_floor() {
-    return currentFloor;
+    return elevio_floorSensor();
 }
 
+void exec_set_last_floor(int floor) {
+    lastFloor = floor;
+}
 
-
+void fsmSetState(ElevatorState newState) {
+    currentState = newState;
+}
 
 
