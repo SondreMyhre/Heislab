63255499b382a33bfb3af1c5f2494be931bf313c
Atle Bystrom <aabystro@stud.ntnu.no>
2025-03-02 21:23:16 +0100

    Debug

--- Prosjekt/source/fsm.h
+++ Prosjekt/source/fsm.h
@@ -1,33 +1,3 @@
-<<<<<<<< HEAD:Project/source/fsm.h
-#ifndef ELEVATOR_FSM_H
-#define ELEVATOR_FSM_H
-
-#include <stdbool.h>
-
-typedef enum {
-    AT_FLOOR,
-    MOVING_UP,
-    MOVING_DOWN,
-    EMERGENCY_STOP_FLOOR,
-    INITIALIZING,
-    EMERGENCY_STOP_SHAFT
-
-
-} ElevatorState;
-
-void elevator_init();           
-void elevator_fsm();             
-void elevator_request_floor(int floor);  
-void elevator_stop();            
-void elevator_clear_stop();     
-void elevator_set_obstruction(bool status); 
-
-ElevatorState elevator_get_state();
-int elevator_get_current_floor();
-void fsmSetState(ElevatorState newState);
-
-#endif 
-========
 #pragma once
 
 
@@ -45,8 +15,7 @@ typedef enum {
 
 void elevator_init();           
 void elevator_fsm();             
-void elevator_request_floor(int floor);  
-void elevator_stop();            
+void elevator_request_floor(int floor);            
 void elevator_clear_stop();     
 void elevator_set_obstruction(int status); 
 void sleepScan(int milliseconds);
@@ -54,4 +23,3 @@ void clearScreen();
 
 ElevatorState elevator_get_state();
 int elevator_get_current_floor();
->>>>>>>> sondre:Prosjekt/source/fsm.h
